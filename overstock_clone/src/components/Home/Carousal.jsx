92d282a2e43446bf5f0a5db443c44caa19b2483f
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-06-15 14:03:39 +0530

    data collected

--- overstock_clone/src/components/Home/Carousal.jsx
+++ overstock_clone/src/components/Home/Carousal.jsx
@@ -4,6 +4,28 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import data from "./data.json";
 
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, background: "black" ,display: "block", }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "black" }}
+      onClick={onClick}
+    />
+  );
+}
+
 const Carousal = () => {
   const settings = {
     infinite: true,
@@ -11,10 +33,26 @@ const Carousal = () => {
     slidesToShow: 4,
     slidesToScroll: 1,
     lazyLoad: true,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2000,
     arrows: true,
     focusOnSelect: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive:[
+      {
+        breakpoint: 480,
+        settings: { slidesToShow: 1, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 2, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 4, slidesToScroll: 1 },
+      },
+    ]
   };
   return (
     <>
@@ -24,7 +62,7 @@ const Carousal = () => {
         <div className="row">
           <Slider {...settings}>
             {data.Home_spring.map((item) => (
-              <div key={item.title} className="col-lg-3 p-3">
+              <div key={item.title} className="col-lg-3 col-md-4 col-sm-6 col-12 p-3">
                 <div className="card" >
                   <img className="card-img-top" src={item.image} alt="Card image cap" />
                   <div className="card-body">
